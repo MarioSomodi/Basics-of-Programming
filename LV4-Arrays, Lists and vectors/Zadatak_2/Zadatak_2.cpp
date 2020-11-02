f5d8e4ac8acb086941dd181036077c299d23030f
MarioSomodi <52134229+MarioSomodi@users.noreply.github.com>
2020-11-02 22:24:04 +0100

    removed personal file path

--- LV4-Arrays, Lists and vectors/Zadatak_2/Zadatak_2.cpp
+++ LV4-Arrays, Lists and vectors/Zadatak_2/Zadatak_2.cpp
@@ -7,9 +7,19 @@ using namespace std;
 
 int main()
 {
-	double *aPolje2 = new double[12];	double s = 0, p;	fstream Zadatak2("C:\\Users\\fpsdr\\Desktop\\Mario_Somodi\\LV4\\OsnoveProgramiranjaLV4\\Datoteke\\Zadatak_2.txt", ios::trunc | ios::out);	for (int i = 0; i < 12; i++)	{		aPolje2[i] = pow(2, i);		Zadatak2 << aPolje2[i] << "  ";		s = s + aPolje2[i];	}	p = s / 12;	Zadatak2 << "\nProsjecna vrijednost  je " << p;	Zadatak2.close();
+	double *aPolje2 = new double[12];
+	double s = 0, p;
+	fstream Zadatak2("Datoteke\\Zadatak_2.txt", ios::trunc | ios::out);
+	for (int i = 0; i < 12; i++)
+	{
+		aPolje2[i] = pow(2, i);
+		Zadatak2 << aPolje2[i] << "  ";
+		s = s + aPolje2[i];
+	}
+	p = s / 12;
+	Zadatak2 << "\nProsjecna vrijednost  je " << p;
+	Zadatak2.close();
 	delete[] aPolje2;
 	system("pause");
 	return 0;
 }
-
